07920ff7ea34e82fa55c1f08d5ff5ed1c24ee4fe
Alexey Ivanov <rbtz@ph34r.me>
2022-01-20 20:46:36 -0800

    examples: added an example showcasing public interfaces

---
+++ examples/simple/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"os"
+
+	seekable "github.com/SaveTheRbtz/zstd-seekable-format-go"
+	"github.com/klauspost/compress/zstd"
+)
+
+func main() {
+	f, err := os.CreateTemp("", "example")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(f.Name())
+
+	w, err := seekable.NewWriter(f, seekable.WithZSTDWOptions(zstd.WithEncoderLevel(zstd.SpeedFastest)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	helloWorld := []byte("Hello World!")
+	// Writer
+	_, err = w.Write(helloWorld)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Closer
+	err = w.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r, err := seekable.NewReader(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Close()
+
+	ello := make([]byte, 4)
+	// ReaderAt
+	r.ReadAt(ello, 1)
+	if !bytes.Equal(ello, []byte("ello")) {
+		log.Fatalf("%+v != ello", ello)
+	}
+
+	world := make([]byte, 5)
+	// Seeker
+	r.Seek(-6, io.SeekEnd)
+	// Reader
+	r.Read(world)
+	if !bytes.Equal(world, []byte("World")) {
+		log.Fatalf("%+v != World", world)
+	}
+
+	// Standard ZSTD Reader
+	f.Seek(0, io.SeekStart)
+	dec, err := zstd.NewReader(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	all, err := io.ReadAll(dec)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !bytes.Equal(all, []byte("Hello World!")) {
+		log.Fatalf("%+v != Hello World!", all)
+	}
+}
