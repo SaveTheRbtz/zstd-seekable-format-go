2fc7e0684c02f12154780027dc64aee2ed7283ac
Alexey Ivanov <rbtz@ph34r.me>
2022-01-22 20:00:33 -0800

    docs: added a dedicated package description

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2022, Alexey Ivanov
+// All rights reserved.
+
+// Package adds ability create ZSTD files in seekable format files
+// and randomly access them using uncompressed offsets.
+package seekable
