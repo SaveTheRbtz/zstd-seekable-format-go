160763d3891eb9311f57ec493bd43f1f375aaefa
Alexey Ivanov <rbtz@ph34r.me>
2022-01-22 20:55:32 -0800

    docs: improve spelling

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2022, Alexey Ivanov
 // All rights reserved.
 
-// Package adds ability create ZSTD files in seekable format files
+// Package adds an ability create ZSTD files in seekable format
 // and randomly access them using uncompressed offsets.
 package seekable
