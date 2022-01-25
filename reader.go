caa576725d7442124baa3589384bed6299e3daa6
Alexey Ivanov <rbtz@ph34r.me>
2022-01-24 19:11:09 -0800

    reader: improve comments

--- reader.go
+++ reader.go
@@ -243,19 +243,19 @@ func (s *ReaderImpl) Close() (err error) {
 
 // FrameOffsetEntry is the post-proccessed view of the Seek_Table_Entries suitable for indexing.
 type FrameOffsetEntry struct {
-	// ID is the is the sequence number of the frame in the index
+	// ID is the is the sequence number of the frame in the index.
 	ID uint64
 
-	// CompOffset is the offset within compressed stream
+	// CompOffset is the offset within compressed stream.
 	CompOffset uint64
-	// DecompOffset is the offset within decompressed stream
+	// DecompOffset is the offset within decompressed stream.
 	DecompOffset uint64
-	// CompSize is the size of the compressed frame
+	// CompSize is the size of the compressed frame.
 	CompSize uint32
-	// DecompSize is the size of the original data
+	// DecompSize is the size of the original data.
 	DecompSize uint32
 
-	// Checksum is the lower 32 bits of the XXH64 hash of the uncompressed data
+	// Checksum is the lower 32 bits of the XXH64 hash of the uncompressed data.
 	Checksum uint32
 }
 
