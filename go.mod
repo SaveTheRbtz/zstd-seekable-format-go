6aa41cbb282a9b63d11d1d6df687b2d17f90ecf7
Alexey Ivanov <rbtz@ph34r.me>
2022-02-05 23:40:49 -0800

    go: bump version to 1.18

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/SaveTheRbtz/zstd-seekable-format-go
 
-go 1.17
+go 1.18
 
 require (
 	github.com/cespare/xxhash v1.1.0
---
+++ seekable_fuzz_test.go
@@ -0,0 +1,87 @@
+//go:build go1.18
+// +build go1.18
+
+package seekable
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"math/rand"
+	"testing"
+
+	"github.com/klauspost/compress/zstd"
+	"github.com/stretchr/testify/assert"
+)
+
+func FuzzRoundTrip(f *testing.F) {
+	dec, err := zstd.NewReader(nil)
+	assert.NoError(f, err)
+	defer dec.Close()
+
+	enc, err := zstd.NewWriter(nil, zstd.WithEncoderLevel(zstd.SpeedFastest))
+	assert.NoError(f, err)
+	defer enc.Close()
+
+	f.Add(int64(1), uint8(0), int16(1), int8(io.SeekStart))
+	f.Add(int64(10), uint8(1), int16(2), int8(io.SeekEnd))
+	f.Add(int64(111), uint8(2), int16(3), int8(io.SeekCurrent))
+
+	f.Fuzz(func(t *testing.T, seed int64, frames uint8, l int16, whence int8) {
+		var b bytes.Buffer
+		bufWriter := bufio.NewWriter(&b)
+
+		w, err := NewWriter(bufWriter, enc)
+		assert.NoError(t, err)
+
+		total := int16(0)
+		rng := rand.New(rand.NewSource(seed))
+		for i := 0; i < int(frames); i++ {
+			sz := rng.Int63n(100)
+			total += int16(sz)
+
+			rndBuf := make([]byte, sz)
+
+			_, err := rng.Read(rndBuf)
+			assert.NoError(t, err)
+
+			_, err = w.Write(rndBuf)
+			assert.NoError(t, err)
+		}
+		err = w.Close()
+		assert.NoError(t, err)
+
+		err = bufWriter.Flush()
+		assert.NoError(t, err)
+
+		r, err := NewReader(bytes.NewReader(b.Bytes()), dec)
+		assert.NoError(t, err)
+
+		off := rng.Int63n(1+4*int64(total)) - 2*int64(total)
+		i, err := r.Seek(off, int(whence))
+		if err != nil {
+			return
+		}
+
+		if l > total || l < 0 {
+			l = total
+		}
+		buf1 := make([]byte, l)
+
+		n, err := r.Read(buf1)
+		if err != nil && err != io.EOF {
+			return
+		}
+
+		buf2 := make([]byte, n)
+		m, err := r.ReadAt(buf2, i)
+		// t.Logf("off: %d, l: %d, whence: %d, i: %d, n: %d, m: %d", off, l, whence, i, n, m)
+
+		if err != io.EOF {
+			assert.NoError(t, err)
+		}
+
+		assert.Equal(t, m, n)
+		assert.Equal(t, buf1[:n], buf2)
+	})
+}
