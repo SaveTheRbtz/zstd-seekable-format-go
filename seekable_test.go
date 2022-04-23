4bf171e895963feefc96c5d7ded3540c8c764528
Alexey Ivanov <rbtz@ph34r.me>
2022-04-23 03:16:42 -0700

    tests: add interoperability tests

--- seekable_test.go
+++ seekable_test.go
@@ -2,6 +2,8 @@ package seekable
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"strconv"
 	"testing"
 
@@ -51,6 +53,54 @@ func TestCreateSkippableFrame(t *testing.T) {
 				assert.NoError(t, err)
 				assert.Equal(t, []byte(nil), decodedeBytes)
 			}
+		})
+	}
+}
+
+func TestIntercompat(t *testing.T) {
+	t.Parallel()
+
+	dec, err := zstd.NewReader(nil)
+	assert.NoError(t, err)
+
+	for _, fn := range []string{
+		// t2sz README.md -l 22 -s 1024 -o testdata/intercompat-t2sz.zst
+		"testdata/intercompat-t2sz.zst",
+		// bazel run //cmd/zstdseek:zstdseek -- \
+		//	-f $(realpath README.md) -o $(realpath testdata/intercompat-zstdseek_v0.zst) \
+		//	-c 1:1 -t -q 13
+		"testdata/intercompat-zstdseek_v0.zst",
+	} {
+		fn := fn
+		t.Run(fn, func(t *testing.T) {
+			t.Parallel()
+
+			f, err := os.Open(fn)
+			assert.NoError(t, err)
+			defer f.Close()
+
+			r, err := NewReader(f, dec)
+			assert.NoError(t, err)
+			defer func() { assert.NoError(t, r.Close()) }()
+
+			buf := make([]byte, 4000)
+			n, err := r.Read(buf)
+			assert.NoError(t, err)
+			assert.Equal(t, n, 1024)
+			assert.Equal(t, []byte("  [![License]"), buf[:13])
+
+			all, err := io.ReadAll(r)
+			assert.NoError(t, err)
+			assert.Greater(t, len(all), 1024)
+
+			i, err := r.Seek(-47, io.SeekEnd)
+			assert.NoError(t, err)
+			assert.Greater(t, i, int64(1024))
+
+			n, err = r.ReadAt(buf, i)
+			assert.ErrorIs(t, err, io.EOF)
+			assert.Equal(t, 47, n)
+			assert.Equal(t, []byte("[license]: https://opensource.org/licenses/MIT\n"), buf[:n])
 		})
 	}
 }
