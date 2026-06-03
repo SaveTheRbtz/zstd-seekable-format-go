f58fac903365f48fa18763d7a25cec46960c87b7
Alexey Ivanov <rbtz@ph34r.me>
2026-06-02 23:30:19 -0700

    document custom frame cache policy

---
+++ pkg/framecache/example_test.go
@@ -0,0 +1,107 @@
+package framecache_test
+
+import (
+	"fmt"
+
+	"github.com/SaveTheRbtz/zstd-seekable-format-go/pkg/framecache"
+)
+
+type clockCache struct {
+	maxFrames int
+	hand      int
+	items     map[framecache.Key]int
+	slots     []clockSlot
+}
+
+type clockSlot struct {
+	key  framecache.Key
+	data []byte
+	used bool
+}
+
+var _ framecache.Cache = (*clockCache)(nil)
+
+func newClockCache(maxFrames int) *clockCache {
+	return &clockCache{
+		maxFrames: maxFrames,
+		items:     make(map[framecache.Key]int),
+	}
+}
+
+func (c *clockCache) Get(key framecache.Key) ([]byte, bool) {
+	index, ok := c.items[key]
+	if !ok {
+		return nil, false
+	}
+	c.slots[index].used = true
+	return c.slots[index].data, true
+}
+
+func (c *clockCache) Put(key framecache.Key, data []byte) {
+	if c.maxFrames <= 0 {
+		return
+	}
+
+	if index, ok := c.items[key]; ok {
+		c.slots[index].data = data
+		c.slots[index].used = true
+		return
+	}
+
+	if len(c.slots) < c.maxFrames {
+		c.items[key] = len(c.slots)
+		c.slots = append(c.slots, clockSlot{key: key, data: data})
+		return
+	}
+
+	for {
+		slot := &c.slots[c.hand]
+		if slot.used {
+			slot.used = false
+			c.advance()
+			continue
+		}
+
+		delete(c.items, slot.key)
+		*slot = clockSlot{key: key, data: data}
+		c.items[key] = c.hand
+		c.advance()
+		return
+	}
+}
+
+func (c *clockCache) advance() {
+	c.hand = (c.hand + 1) % len(c.slots)
+}
+
+func ExampleCache_customReplacementPolicy() {
+	var cache framecache.Cache = newClockCache(2)
+
+	first := framecache.NewKey(1, 1)
+	second := framecache.NewKey(1, 2)
+	third := framecache.NewKey(1, 3)
+
+	cache.Put(first, []byte("first"))
+	cache.Put(second, []byte("second"))
+
+	_, _ = cache.Get(first)
+	cache.Put(third, []byte("third"))
+
+	printFrame(cache, first)
+	printFrame(cache, second)
+	printFrame(cache, third)
+
+	// Output:
+	// first
+	// miss
+	// third
+}
+
+func printFrame(cache framecache.Cache, key framecache.Key) {
+	data, ok := cache.Get(key)
+	if !ok {
+		fmt.Println("miss")
+		return
+	}
+	fmt.Println(string(data))
+}
